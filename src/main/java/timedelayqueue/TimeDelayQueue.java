0fdcb202429b4ef6a77e5d27992ee7bd19ed3001
Tony Li <2906681472@qq.com>
2022-11-27 18:05:49 -0800

    task 1+2 initial attempt

--- src/main/java/timedelayqueue/TimeDelayQueue.java
+++ src/main/java/timedelayqueue/TimeDelayQueue.java
@@ -1,6 +1,6 @@
 package timedelayqueue;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
 import java.util.*;
 
 // TODO: write a description for this class
@@ -15,9 +15,9 @@ public class TimeDelayQueue {
 
     private final int delay;
 
-    private Queue<TimestampedObject> queue;
+    private final Queue<PubSubMessage> queue;
 
-    private List<Timestamp> history;
+    private final List<Timestamp> history;
 
     private long totalMsgCount;
 
@@ -34,7 +34,7 @@ public class TimeDelayQueue {
      */
     public TimeDelayQueue(int delay) {
         this.delay = delay;
-        queue = new PriorityQueue<>();
+        queue = new PriorityQueue<>(new PubSubMessageComparator());
         history = new ArrayList<>();
         totalMsgCount = 0L;
     }
@@ -43,22 +43,50 @@ public class TimeDelayQueue {
     // if a message with the same id exists then
     // return false
     public boolean add(PubSubMessage msg) {
-        return false;
+        synchronized (TimeDelayQueue.class){
+            removeExpiredTransientMsg();
+            Timestamp now = new Timestamp(System.currentTimeMillis());
+            if (queue.contains(msg)) {
+                return false;
+            } else {
+                queue.add(msg);
+                history.add(now); // record the action
+                totalMsgCount ++; // update message count
+            }
+            return true;
+        }
     }
 
     /**
      * Get the count of the total number of messages processed
      * by this TimeDelayQueue
-     * @return
+     * @return totalMsgCount
      */
     public long getTotalMsgCount() {
-        return -1;
+        return totalMsgCount;
     }
 
     // return the next message and PubSubMessage.NO_MSG
-    // if there is ni suitable message
+    // if there is no suitable message
     public PubSubMessage getNext() {
-        return PubSubMessage.NO_MSG;
+        synchronized (TimeDelayQueue.class){
+            Timestamp now = new Timestamp(System.currentTimeMillis());
+            removeExpiredTransientMsg();
+            PubSubMessage next;
+            try {
+                next = queue.element();
+            } catch (NoSuchElementException e){
+                return PubSubMessage.NO_MSG;
+            }
+
+            if ((next.getTimestamp().getTime() + delay) >= now.getTime()){
+                queue.remove(next);
+                history.add(now);
+                return next;
+            } else {
+                return PubSubMessage.NO_MSG;
+            }
+        }
     }
 
     // return the maximum number of operations
@@ -66,7 +94,38 @@ public class TimeDelayQueue {
     // any window of length timeWindow
     // the operations of interest are add and getNext
     public int getPeakLoad(int timeWindow) {
-        return -1;
+        int peakLoad = 0;
+        history.sort(Timestamp::compareTo);
+        for (int i = 0; i < history.size(); i++) {
+            long initialTime = history.get(i).getTime();
+            int currentPeakLoad = 0;
+            for (int j = i; j < history.size(); j++) {
+                if (history.get(j).getTime() - initialTime <= timeWindow){
+                    currentPeakLoad ++;
+                }
+            }
+            if (currentPeakLoad > peakLoad) {
+                peakLoad = currentPeakLoad;
+            }
+        }
+        return peakLoad;
+    }
+
+    /**
+     * remove expired transient messages
+     */
+    private void removeExpiredTransientMsg() {
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        List<PubSubMessage> toBeRemoved = new ArrayList<>();
+        queue.forEach(msg -> {
+            if (msg.isTransient()) {
+                long expireTime = msg.getTimestamp().getTime() + ((TransientPubSubMessage) msg).getLifetime();
+                if (expireTime > now.getTime()){
+                    toBeRemoved.add(msg);
+                }
+            }
+        });
+        queue.removeAll(toBeRemoved);
     }
 
 }
